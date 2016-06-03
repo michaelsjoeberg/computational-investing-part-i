569e66e2bd4108ce2cbd9b6704c1faa64a674a13
Michael Sjoeberg <mail@michaelsjoeberg.com>
2016-06-03 22:58:43 +0200

    base

--- Digging into Data/marketsim.py
+++ Digging into Data/marketsim.py
@@ -1,15 +1,15 @@
-# import pandas as pd
+import pandas as pd
 import csv
 import sys
 import numpy as np
 import math
 import copy
-# import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.qsdateutil as du
 import time
 import datetime as dt
-# import QSTK.qstkutil.DataAccess as da
-# import QSTK.qstkutil.tsutil as tsu
-# import QSTK.qstkstudy.EventProfiler as ep
+import QSTK.qstkutil.DataAccess as da
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkstudy.EventProfiler as ep
 
 def read_file(filename):
 	# create lists for symbols and dates
@@ -25,30 +25,84 @@ def read_file(filename):
 
 	# remove duplicates
 	list_symbols = list(set(list_symbols))
+	list_dates = list(set(list_dates))
 
 	return list_symbols, list_dates
 	# print list_symbols, list_dates
 
 if __name__ == '__main__':
 	# set arguments as variables
-	starting_cash = str(sys.argv[1])
-	file_orders = str(sys.argv[2])
-	file_value = str(sys.argv[3])
+	# starting_cash = str(sys.argv[1])
+	# file_orders = str(sys.argv[2])
+	# file_value = str(sys.argv[3])
+
+	starting_cash = "1000000"
+	file_orders = "orders.csv"
+	file_value = "values.csv"
 
 	# read orders file
 	ls_symbols, ls_dates = read_file(file_orders)
 
+	# set start and end date for trades
 	dt_start = ls_dates[0]
 	dt_end = ls_dates[-1] + dt.timedelta(days=1)
 
-    # ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
+	# get data
+	ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
+	dataobj = da.DataAccess('Yahoo')
+	ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+	ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+	d_data = dict(zip(ls_keys, ldf_data))
 
-    # dataobj = da.DataAccess('Yahoo')
-    # ls_symbols = dataobj.get_symbols_from_list('sp5002012')
-    # ls_symbols.append('SPY')
+	# create array of adjusted close prices and normalize
+	na_price = d_data['close']
+	#na_normalized_price = na_price / na_price[0, :]
 
-    # ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
-    # ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
-    # d_data = dict(zip(ls_keys, ldf_data))
+	# trade matrix
+	trade_matrix = {ls_symbols[0] : np.zeros(len(ls_dates)),
+					ls_symbols[1] : np.zeros(len(ls_dates)),
+					ls_symbols[2] : np.zeros(len(ls_dates)),
+					ls_symbols[3] : np.zeros(len(ls_dates)),
+					}
+
+	trade_matrix = pd.DataFrame(trade_matrix, index=ls_dates)
+
+	# allocations of stocks
+	allocations = {ls_symbols[0] : 0.0,
+					ls_symbols[1] : 0.0,
+					ls_symbols[2] : 0.0,
+					ls_symbols[3] : 0.0,
+					}
+
+	allocations = pd.DataFrame(allocations, index=ls_dates)
+
+	# total cash
+	total_cash = int(starting_cash)
+
+	# read csv file again
+	reader = csv.reader(open(file_orders, 'rU'), delimiter=',')
+	for row in reader:
+
+		# set date for trade
+		date = dt.datetime(int(row[0]), int(row[1]), int(row[2]))
+		timestamp = du.getNYSEdays(date, date + dt.timedelta(days=1) , dt.timedelta(hours=16))
+
+		# set price for traded stock
+		temp_data = dataobj.get_data(timestamp, [row[3]], ['close'])
+		price_data = dict(zip(['close'], temp_data))
+		price = price_data['close'].values
+		price = int(price[0])
+
+		if (row[4] == "Buy"):
+			allocations[row[3]] = allocations[row[3]] + int(row[5])
+			trade_matrix.set_value(date, row[3], trade_matrix.get_value(date, row[3]) + int(row[5]))
+			total_cash = total_cash - (int(row[5]) * price)
+
+		elif (row[4] == "Sell"):
+			allocations[row[3]] = allocations[row[3]] - int(row[5])
+			trade_matrix.set_value(date, row[3], trade_matrix.get_value(date, row[3]) - int(row[5]))
+			total_cash = total_cash + (int(row[5]) * price)
 
 
+	print na_price["2011-02-02"]
+	print allocations["2011-02-02"]
